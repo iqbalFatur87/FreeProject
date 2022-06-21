ce05e63303029cfbc6e053bda9b008d45217ada0
Iqbal <Iqbal@DESKTOP-L99PGHG>
2022-06-21 18:39:05 +0700

    Add project files.

---
+++ FreeProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreeProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
